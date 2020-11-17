c55a7dc1ecc9a66cc286cb26947fc9c9e2bee149
nikitabray <nikita.tolstokorov@gmail.com>
2020-11-17 20:24:10 +0300

    .

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 def app(environ, start_response):
-    data = environ['QUERY_STRING'][2:].split('&')
+    data = [bytes(i + '\n', 'utf8')
+            for i in environ['QUERY_STRING'][2:].split('&')]
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
     ])
-    return iter([data])
+    return data
 
 # print(app({'QUERY_STRING': '/?a=1&a=2&b=3'}))
