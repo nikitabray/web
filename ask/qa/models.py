d2f87948c898e4f2d1aa76e4f5a2ef1e8ee3a313
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-11 23:24:56 +0300

    fix

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,8 +7,7 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField(blank=True)
-    added_at = models.DateTimeField(verbose_name='Дата публикации',
-                                    auto_now_add=True)
+    added_at = models.DateTimeField(verbose_name='Дата публикации', auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.CharField(max_length=80)
     likes = models.ManyToManyField(User, related_name='question_like_user')
