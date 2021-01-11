4a16a1203e30a9b8e6989428fc0ae9129fa9346a
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-11 12:37:33 +0300

    quickfix

---
+++ ask/qa/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.urls import path
+from .views import test
+
+
+urlpatterns = [
+    path('', test, name='home'),
+    path('login/', test, name='login'),
+    path('signup', test, name='signup'),
+    path('question/<int:question_id>', test, name='question'),
+    path('ask', test, name='ask'),
+    path('popular', test, name='popular'),
+    path('new', test, name='new'),
+]
