f724f6633ffab3e27889e4be8d83c8801810b4a9
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-11 23:30:12 +0300

    fix

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,13 +7,13 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField(blank=True)
-    added_at = models.DateTimeField(verbose_name='Дата публикации', auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.CharField(max_length=80)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
     class Meta:
-        verbose_name = 'Вопрос'
+        verbose_name = 'Question'
         ordering = ['-added_at']
 
     class QuestionManager(models.Manager):
@@ -26,8 +26,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(verbose_name='Дата публикации',
-                                    auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.OneToOneField(
         Question,
         on_delete=models.CASCADE,
