87177c6fa2e73208bdbfdf248c482d6ec53e28d8
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-11 13:40:13 +0300

    django 3.1.5 => 2.1

--- ask/ask/asgi.py
+++ ask/ask/asgi.py
@@ -4,7 +4,7 @@ ASGI config for ask project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/asgi/
 """
 
 import os
--- ask/qa/apps.py
+++ ask/qa/apps.py
@@ -3,4 +3,3 @@ from django.apps import AppConfig
 
 class QaConfig(AppConfig):
     name = 'qa'
-    verbose_name = 'Вопросы & Ответы'
