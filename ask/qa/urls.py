62aae4d6a264f399a9c509e782dd69ed3ddb8ec5
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-11 14:01:17 +0300

    urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -6,9 +6,9 @@ from .views import test
 urlpatterns = [
     path('', test, name='home'),
     path('login/', test, name='login'),
-    path('signup', test, name='signup'),
-    path('question/<int:question_id>', test, name='question'),
-    path('ask', test, name='ask'),
-    path('popular', test, name='popular'),
-    path('new', test, name='new'),
+    path('signup/', test, name='signup'),
+    path('question/<int:question_id>/', test, name='question'),
+    path('ask/', test, name='ask'),
+    path('popular/', test, name='popular'),
+    path('new/', test, name='new'),
 ]
