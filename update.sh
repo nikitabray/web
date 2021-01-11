ddc1bb4d320770bd551e70c8668c16b92dd65e6a
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-11 23:10:33 +0300

    op

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ sudo apt install python3.5
 sudo apt install python3.5-dev
 sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo python3 -m pip install gunicorn
-sudo python3 -m install django==2.0
-sudo python3 -m pip install mysqlclient
+sudo pip3 install gunicorn
+sudo pip3 install django==2.0
+sudo pip3 install mysqlclient
