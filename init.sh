77d4e78813de39c4a0c99371a45b9b80222ef42f
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-14 11:39:25 +0300

    fix

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
 cd /home/box/web  
 sudo nginx -c /home/box/web/etc/nginx.conf  
-sudo gunicorn -c /home/box/web/etc/gunicorn_hello.conf --bind 0.0.0.0:8080 hello:app &
-cd ask 
-sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application &
+cd /home/box/web/ask/manage.py runserver 0.0.0.0:8000
