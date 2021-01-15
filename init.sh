f72adbde0ae2a54ed389f03e0b0185e3b0d04832
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-15 19:07:49 +0300

    fix

--- init.sh
+++ init.sh
@@ -6,13 +6,15 @@ sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo pip3 install gunicorn
 sudo pip3 install django==2.0
 sudo pip3 install mysqlclient
+sudo /etc/init.d/mysql start
+
+mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+python3 ~/web/ask/manage.py makemigrations
+python3 ~/web/ask/manage.py migrate
 
 cd /home/box/web  
 sudo nginx -c /home/box/web/etc/nginx.conf  
 python3 /home/box/web/ask/manage.py runserver 0.0.0.0:8000
 
-sudo /etc/init.d/mysql start
-mysql -uroot -e "create database stepic_web;"
-mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
-python3 ~/web/ask/manage.py makemigrations
-python3 ~/web/ask/manage.py migrate
+
