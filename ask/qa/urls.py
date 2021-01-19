a428b0868b67d34700016b28fa14bdade1f49eb8
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-19 12:51:04 +0300

    fix

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', test, name='login'),
     path('signup/', test, name='signup'),
     path('question/<int:question_id>/', question, name='question'),
-    path('ask/', test, name='ask'),
+    path('ask/', postform , name='ask'),
     path('popular/', popular, name='popular'),
     path('new/', test, name='new'),
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -17,6 +17,7 @@ def mainpage(request):
     context = {'page': page, 'posts': page.object_list}
     return render(request, 'index.html', context)
 
+
 def popular(request):
     paginator = Paginator(Question.objects.popular(), 10)
     page = request.GET.get('page', 1)
@@ -25,11 +26,13 @@ def popular(request):
     context = {'page': page, 'posts': page.object_list}
     return render(request, 'index.html', context)
 
+
 def question(request, question_id):
     question = Question.objects.get(pk=question_id)
     answer = Answer.objects.filter(question=question)
     context = {'question': question, 'answer': answer}
     return render(request, 'question.html', context)
+
 
 def postform(request):
     if request.method == 'POST':
@@ -37,4 +40,6 @@ def postform(request):
         if form.is_valid():
             question = form.save()
             return HttpResponseRedirect('/question/' + str(question.id))
-
+        else:
+            form = AskForm()
+        return render(request, 'add_post.html', {'form': form})
