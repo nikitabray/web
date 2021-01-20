602ffd6bdb7caf5eaa6df2ea3684c973f59cf943
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-20 12:51:50 +0300

    fix

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -20,7 +20,7 @@ class AnswerForm(forms.ModelForm):
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1')
+        fields = ('username', 'email', 'password')
 
 
 
