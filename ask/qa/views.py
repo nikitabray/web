54346cfed0df256b2402b28c2ebf6a29d752908e
nikitabray <nikita.tolstokorov@gmail.com>
2021-01-21 18:27:17 +0300

    fix

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -72,7 +72,7 @@ def login_to_site(request):
             if user:
                 login(request, form.get_user())
         response = HttpResponseRedirect('home')
-        response.set_cookie('sessionid', request.session) 
+        response.set_cookie('sessionid', 'sasa') 
         return response
     else:
         form = AuthenticationForm()
